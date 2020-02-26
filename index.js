0ce5657464189daf3bd1e21cb3bc1ff6aa328ab2
Shawn Wright <volushawn@gmail.com>
2020-02-25 23:19:35 -0600

    Built out prompt questions

--- index.js
+++ index.js
@@ -6,7 +6,15 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 const questions = [
     "What is your Github username?", 
-    "Do you like me?"
+    "What is the title of this project?",
+    "What is the description of this project?",
+    "What is the Table of Contents for this project?",
+    "What is the Installation guide for this project? (leave empty if none)",
+    "What is the usage guide for this project?",
+    "License?",
+    "Contributing authors?",
+    "Tests for this project?",
+    "Questions"
 
 ];
 
@@ -24,12 +32,62 @@ function init() {
             {
                 type: "input",
                 message: questions[1],
-                name: "likeMe"
+                name: "title"
+            },
+            {
+                type: "input",
+                message: questions[2],
+                name: "description"
+            },
+            {
+                type: "input",
+                message: questions[3],
+                name: "toc"
+            },
+            {
+                type: "input",
+                message: questions[4],
+                name: "installation"
+            },
+            {
+                type: "input",
+                message: questions[5],
+                name: "usage"
+            },
+            {
+                type: "list",
+                message: questions[6],
+                name: "license",
+                choices: [
+                    "MIT",
+                    "ISC",
+                    "GNU GPLv3",
+                    "GNU AGPLv3",
+                    "Mozilla Public License 2.0",
+                    "The Unlicense"
+                ]
+            },
+            {
+                type: "input",
+                message: questions[7],
+                name: "contributors"
+            },
+            {
+                type: "input",
+                message: questions[8],
+                name: "tests"
+            },
+            {
+                type: "input",
+                message: questions[9],
+                name: "questions"
             },
         ])
         .then(function (response) {
+            //get Github into
             let user = api.getUser(response.username);
             console.log(user);
+            
         });
 }
 
