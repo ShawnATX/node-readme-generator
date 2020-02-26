a66b43a13c14c845e520476b0f078fbe510a2d36
Shawn Wright <volushawn@gmail.com>
2020-02-25 22:22:39 -0600

    API returning data from Github

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const api = require("./utils/api");
+const generateMarkdown = require("./utils/generateMarkdown");
+
+
 const questions = [
+    "What is your Github username?", 
+    "Do you like me?"
 
 ];
 
@@ -6,7 +14,22 @@ function writeToFile(fileName, data) {
 }
 
 function init() {
-
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: questions[0],
+                name: "username"
+            },
+            {
+                type: "input",
+                message: questions[1],
+                name: "likeMe"
+            },
+        ])
+        .then(function (response) {
+            console.log(api.getUser(response.username));
+        });
 }
 
 init();
--- utils/api.js
+++ utils/api.js
@@ -1,8 +1,26 @@
+const axios = require('axios');
+
+
 const api = {
-    getUser(username) {
-  
-    }
-  };
-  
-  module.exports = api;
-  
+  //connect to github API to retrieve user information
+      getUser: async function getUser(username) {
+        try {
+          const response = await axios.get('https://api.github.com/users/' + username);
+          let userObj = {
+            username: response.data.login,
+            email: response.data.email,
+            avatar: response.data.avatar_url
+          }
+          //console.log(userObj);
+          return userObj;
+        } catch (error) {
+          if (error.response.status === '404'){
+            console.log("Username not found");
+          }
+        }
+      }
+
+    };
+    
+    module.exports = api;
+    
